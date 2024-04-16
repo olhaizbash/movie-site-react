c7ac2ee4d4ecff757a3b5c2bf0a88e80a7319330
Olha Izbash <olgaizbash@gmail.com>
2024-04-16 19:24:40 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,48 @@
+import { Container } from "../Container/Container";
+import sprite from "../../images/sprite.svg";
+import {
+  FooterWrapper,
+  TextFooterWrapper,
+  TextFooter,
+  FooterIconLove,
+  FooterList,
+  FooterLink,
+  FooterIconSocial,
+  FooterSection,
+} from "./Footer.styled";
+
 const Footer = () => {
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <FooterSection>
+      <Container>
+        <FooterWrapper>
+          <TextFooterWrapper>
+            <TextFooter>2024 | Developed with </TextFooter>
+            <FooterIconLove>
+              <use href={`${sprite}#icon-heart`}></use>
+            </FooterIconLove>
+          </TextFooterWrapper>
+          <FooterList>
+            <li>
+              <FooterLink
+                href="https://www.linkedin.com/in/olha-izbash"
+                target="_blank">
+                <FooterIconSocial>
+                  <use href={`${sprite}#icon-inkedin`}></use>
+                </FooterIconSocial>
+              </FooterLink>
+            </li>
+            <li>
+              <FooterLink href="https://github.com/olhaizbash" target="_blank">
+                <FooterIconSocial>
+                  <use href={`${sprite}#icon-github`}></use>
+                </FooterIconSocial>
+              </FooterLink>
+            </li>
+          </FooterList>
+        </FooterWrapper>
+      </Container>
+    </FooterSection>
   );
 };
 
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const FooterSection = styled.section`
+  padding-bottom: 20px;
+`;
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const TextFooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const TextFooter = styled.p`
+  color: ${({ theme }) => theme.color.textchange};
+  text-align: center;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const FooterIconLove = styled.svg`
+  width: 24px;
+  height: 24px;
+`;
+
+export const FooterIconSocial = styled.svg`
+  width: 24px;
+  height: 24px;
+  fill: ${({ theme }) => theme.color.logoText};
+  transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    fill: ${({ theme }) => theme.color.orange};
+  }
+`;
+
+export const FooterList = styled.ul`
+  display: flex;
+  gap: 10px;
+`;
+
+export const FooterLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+`;
